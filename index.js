46b3f06f60f62ce31127cbedf2682224d05b0e91
astrupp <adamstrupp@gmail.com>
2016-08-25 13:51:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_dxbxt5mh:kc27o9g2huidb6a2uubv3fhe6t@ds031611.mlab.com:31611/heroku_dxbxt5mh',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'vegaswinnersklejrjk3gljgojllogjlojg',
+  masterKey: process.env.MASTER_KEY || 'kh2fbehfb2kbfkhjbfk',
+  serverURL: process.env.SERVER_URL || 'https://vegas-sports-winners1.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
